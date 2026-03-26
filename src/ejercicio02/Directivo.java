3b739824b817237cc264666235318052f2f81075
salvadorpulido-commits <salvador.pulido@iesnervion.es>
2026-03-26 13:24:41 +0100

    correcciojn

--- src/ejercicio02/Directivo.java
+++ src/ejercicio02/Directivo.java
@@ -4,12 +4,12 @@ public class Directivo extends Empleado {
 
     // Constructor
     public Directivo(String nombre) {
-        super(nombre); // Llama al constructor de Empleado
+        super(nombre); // Llama al constructor Empleado
     }
 
     @Override
     public String toString() {
-        // Aprovecha el toString de Empleado y le concatena su parte
+        // toString de Empleado y concatena su parte
         return super.toString() + " -> Directivo";
     }
 }
