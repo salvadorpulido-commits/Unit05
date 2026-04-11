700dc37926e492e292d6bf29510f758f4d222dfe
salva <salva@Katana>
2026-04-12 01:20:30 +0200

    ejercicio analisis biblioteca

--- src/ejercicioAnalisis02Biblioteca/DVD.java
+++ src/ejercicioAnalisis02Biblioteca/DVD.java
@@ -61,3 +61,4 @@ public class DVD extends Ficha {
                 + ", tipo=" + tipo + "]";
     }
 }
+
--- src/ejercicioAnalisis02Biblioteca/Libro.java
+++ src/ejercicioAnalisis02Biblioteca/Libro.java
@@ -39,3 +39,4 @@ public class Libro extends Ficha {
                 + ", editorial=" + editorial + "]";
     }
 }
+
